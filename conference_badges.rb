8899e166251f05994194954f48354b87a7be99f7
Youngmin Park <codepark2019@gmail.com>
2020-05-28 23:26:17 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -17,6 +17,9 @@ def assign_rooms(attendees)
 end
 #
 def printer(names)
+  badge_maker(name).each do |badge|
+    puts badge
+  end
 #   batch_badge_creator(names).each do |badge|
 #     puts badge
 #   end
