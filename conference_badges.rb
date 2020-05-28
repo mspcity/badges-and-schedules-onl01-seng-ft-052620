6ee48acdfaab01835d9f80fba43275c7fc54ba5e
Youngmin Park <codepark2019@gmail.com>
2020-05-28 23:18:43 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -10,7 +10,7 @@ def batch_badge_creator(names)
   end
 end
 #
-def assign_rooms
+def assign_rooms(attendees)
 #   attendees.map.with_index(1) do |attendee, index|
 #      "Hello, #{attendee}! You'll be assigned to room #{index}!"
 #   end
